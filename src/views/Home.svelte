cee5037ad5e2a5634e41209a7c888c1604a88b9c
dr3 <dr3@dr3.com>
2022-07-25 19:58:24 +0700

    Upd

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -1,6 +1,9 @@
 <script>
+    let state = {}
+    let ws = null;
+
     function startWs() {
-        const ws = new WebSocket('ws://127.0.0.1:8000?token=Y2xpZW50OmxtYW8=');
+        ws = new WebSocket('ws://168.119.247.99:8000?token=Y2xpZW50OmxtYW8=');
 
         ws.addEventListener('open', function (event) {
             console.log("It's open");
@@ -22,13 +25,31 @@
         ws.addEventListener('message', function (event) {
             //console.log('Got', event.data)
             //let d = JSON.parse(event.data)
-            console.log(Buffer.from(JSON.parse(event.data)).toString())
-
-        ws.send(JSON.stringify({ping: "back"}), {binary: false})
+            try {
+                let json = JSON.parse(event.data);
+                //console.log(x.humi)
+                state = json;
+                ws.send(JSON.stringify({ping: "back"}), {binary: false})
+            }
+            catch (e) { console.log("Couldnt parse WS message")}
 
         });
     }
     startWs()
+
+    function setPPM(val) {
+        console.log("Setting ppm to", val)
+        ws.send(JSON.stringify({command: "setPPM", value: 700}))
+    }
 </script>
 
-<main class="row container my-50 gap-40 grow">GROW HOSUE</main>
+<main class="row container my-50 gap-40 grow">
+    GROW HOUSE
+
+    {#if state.humi}
+        <div>Humidity: {state.humi.toFixed(2)}</div>
+        <div>Temperature (C): {state.tempC.toFixed(2)}</div>
+        <div>Current PPM {state.currentPPM.toFixed(2)}</div>
+        <div on:click={setPPM}>Set PPM</div>
+    {/if}
+</main>
