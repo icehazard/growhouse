3c3d48102297a1dd96a6915843561313e1350738
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2022-07-27 13:41:10 +0700

    ws

--- src/store/ws.js
+++ src/store/ws.js
@@ -15,28 +15,33 @@ context.setPh = function (ph) {
 
 export default context
 
-let ws = new WebSocket("ws://168.119.247.99:8000?token=Y2xpZW50OmxtYW8=");
+function start() {
+    let ws = new WebSocket("ws://168.119.247.99:8000?token=Y2xpZW50OmxtYW8=");
 
-ws.addEventListener("open", function (event) {
-    console.log("WS connected");
-});
+    ws.addEventListener("open", function (event) {
+        console.log("WS connected");
+    });
 
-ws.addEventListener("close", function (event) {
-    console.log("Encountered close, trying to reconnect");
-    setTimeout(startWs, 5e3);
-});
+    ws.addEventListener("close", function (event) {
+        console.log("Encountered close, trying to reconnect");
+        setTimeout(start, 5e3);
+    });
 
-ws.addEventListener("error", function (event) {
-    console.log("Encountered error, trying to reconnect");
-    setTimeout(startWs, 5e3);
-});
+    ws.addEventListener("error", function (event) {
+        console.log("Encountered error, trying to reconnect");
+        setTimeout(start, 5e3);
+    });
 
-ws.addEventListener("message", function (event) {
-    try {
-        let json = JSON.parse(event.data);
-        ws.send(JSON.stringify({ ping: "back" }), { binary: false });
-        context.commit('ws', json)
-    } catch (e) {
-        console.log("Couldnt parse WS message");
-    }
-});
+    ws.addEventListener("message", function (event) {
+        try {
+            let json = JSON.parse(event.data);
+            ws.send(JSON.stringify({ ping: "back" }), { binary: false });
+            context.commit('ws', json)
+        } catch (e) {
+            console.log("Couldnt parse WS message");
+        }
+    });
+}
+
+start()
+
