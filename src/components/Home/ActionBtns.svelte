bd6231d93bdfc7cfc1ee2889b8f22d302f49b79f
dr3 <dr3@dr3.com>
2022-09-17 16:02:53 +0700

    Upd

--- src/components/Home/ActionBtns.svelte
+++ src/components/Home/ActionBtns.svelte
@@ -3,6 +3,7 @@
     import Icon from "@iconify/svelte";
     import Toggle from "svelte-toggle";
     import dayjs from "dayjs"
+
     var relativeTime = require('dayjs/plugin/relativeTime')
     dayjs.extend(relativeTime)
 
@@ -24,7 +25,7 @@
             for (let i = 0; i < 24; i += $ws.ws.state.FEED_EVERY_X_HOURS) {
                 hoursArray.push(i)
 
-                let next = start.add(i+3, 'hour')
+                let next = start.add(i + 3, 'hour')
                 nextFeeds.push(next.fromNow())
                 //console.log(next.fromNow())
             }
@@ -36,21 +37,26 @@
     function runCommand(cmd) {
         ws.cmd(cmd);
     }
+
     function runCommandMiddleman(cmd) {
         ws.cmdMiddleman(cmd);
     }
+
     function flipSchedule() {
         //feedScheduleOn = !feedScheduleOn;
     }
+
     // $: {
     //     feedScheduleOn, al()
     // }
     function adjustSchedule() {
         ws.cmdMiddleman(feedScheduleOn ? "feedScheduleOff" : "feedScheduleOn");
     }
+
     function adjustPH() {
         ws.cmdMiddleman(adjustPHOn ? "adjustPHOff" : "adjustPHOn");
     }
+
     function adjustPPM() {
         ws.cmdMiddleman(adjustPPMOn ? "adjustPPMOff" : "adjustPPMOn");
     }
@@ -59,63 +65,66 @@
 <div class="row gap-20 shade1 border curve wrap pa-20 grow center">
     <div class="col center gap-10">
         <button
-            class="h-100 shade3 w-100 center curve shadow fast shine"
-            value="RESTART"
-            on:click={() => runCommand("restart")}
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                value="RESTART"
+                on:click={() => runCommand("restart")}
         >
-            <Icon color={col} icon="ic:twotone-restart-alt" height="30" />
+            <Icon color={col} icon="ic:twotone-restart-alt" height="30"/>
         </button>
         <span class="font-14 opacity-75">Restart</span>
     </div>
     <div class="col center gap-10">
         <button
-            class="h-100 shade3 w-100 center curve shadow fast shine"
-            value="1MIN FEED"
-            on:click={() => runCommandMiddleman("minfeed")}
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                value="1MIN FEED"
+                on:click={() => runCommandMiddleman("minfeed")}
         >
-            <Icon color={col} icon="akar-icons:bell" width="25" />
+            <Icon color={col} icon="akar-icons:bell" width="25"/>
         </button>
         <span class="font-14 opacity-75">1MIN FEED</span>
     </div>
     <div class="col center gap-10">
-        <button class="h-100 shade3 w-100 center curve shadow fast shine" on:click={() => runCommand("on") } class:borderPrimary={$ws.ws.RO_ON}>
-            <Icon color={col} icon="mdi:water-plus-outline" width="30" />
+        <button class="h-100 shade3 w-100 center curve shadow fast shine" on:click={() => runCommand("on") }
+                class:borderPrimary={$ws.ws.RO_ON}>
+            <Icon color={col} icon="mdi:water-plus-outline" width="30"/>
         </button>
         <span class="font-14 opacity-75">Refill Tank</span>
     </div>
     <div class="col center gap-10">
         <button class="h-100 shade3 w-100 center curve shadow fast shine" on:click={() => runCommand("feedon")}>
-            <Icon color={col} icon="ant-design:info-circle-outlined" width="30" />
+            <Icon color={col} icon="ant-design:info-circle-outlined" width="30"/>
         </button>
         <span class="font-14 opacity-75">FEEDON</span>
     </div>
     <div class="col center gap-10">
         <button
-            class="h-100 shade3 w-100 center curve shadow fast shine"
-            on:click={() => runCommand("fetchConfig")}
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                on:click={() => runCommand("fetchConfig")}
         >
-            <Icon color={col} icon="ion:cog-sharp" width="30" />
+            <Icon color={col} icon="ion:cog-sharp" width="30"/>
         </button>
         <span class="font-14 opacity-75">FETCH CONF</span>
     </div>
     <div class="col center gap-10">
         <button
-            class="h-100 shade3 w-100 center curve shadow fast shine"
-            on:click={() => runCommand("romoff")}
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                on:click={() => runCommand("romoff")}
         >
-            <Icon color={col} icon="healthicons:running-water-outline" width="30" />
+            <Icon color={col} icon="healthicons:running-water-outline" width="30"/>
         </button>
         <span class="font-14 opacity-75">ROMOFF</span>
     </div>
     <div class="col center gap-10">
         <button
-            class="h-100 shade3 w-100 center curve shadow fast shine"
-            on:click={() => runCommandMiddleman("refill")}
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                on:click={() => runCommandMiddleman("refill")}
         >
-            <Icon color={col} icon="fluent:food-20-regular" width="30" />
+            <Icon color={col} icon="fluent:food-20-regular" width="30"/>
         </button>
         <span class="font-14 opacity-75">REFILL</span>
     </div>
+</div>
+<div class="row gap-20 shade1 border curve wrap pa-20 grow center">
     <div class="col center gap-10">
         {#if $ws.ws && $ws.ws.state && $ws.ws.state.hasOwnProperty("FEED_STATE")}
             <Toggle
@@ -129,7 +138,7 @@
                     on:click={adjustSchedule}
             />
             Feeding hours are: {hoursArray} <br/>
-            Feed duration is set to {$ws.ws.state.RUN_DURATION/1000} secs <br/>
+            Feed duration is set to {$ws.ws.state.RUN_DURATION / 1000} secs <br/>
             Next feed ~{nextFeed}
         {/if}
     </div>
@@ -173,7 +182,7 @@
     button:active {
         box-shadow: none;
         filter: brightness(0.98);
-      
+
     }
 
 
