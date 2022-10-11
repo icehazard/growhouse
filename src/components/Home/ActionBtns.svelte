aa753db0069d7a31c09bf841edccd9dff93bf941
dr3 <dr3@dr3.com>
2022-10-11 11:18:31 +0700

    Upd

--- src/components/Home/ActionBtns.svelte
+++ src/components/Home/ActionBtns.svelte
@@ -127,6 +127,15 @@
         </button>
         <span class="font-14 opacity-75">REFILL</span>
     </div>
+    <div class="col center gap-10">
+        <button
+                class="h-100 shade3 w-100 center curve shadow fast shine"
+                on:click={() => runCommandMiddleman("bottle")}
+        >
+            <Icon color={col} icon="fluent:food-20-regular" width="30"/>
+        </button>
+        <span class="font-14 opacity-75">REF BOTTLE</span>
+    </div>
     <div class="row gap-20 shade1 border curve wrap pa-20 grow center">
         <div class="col center gap-10">
             {#if $ws.ws && $ws.ws.state && $ws.ws.state.hasOwnProperty("ADJUST_PPM")}
