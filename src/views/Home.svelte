b20fb2e4770a5fb43f9817367b28c4778669943c
Sebastian Whitlock <sebastian.whitlock@securcom.me>
2023-02-06 21:12:14 +0700

    new ui

--- src/views/Home.svelte
+++ src/views/Home.svelte
@@ -2,20 +2,27 @@
     import PPMcontroller from "comp//Home/PPMcontroller.svelte";
     import Sensors from "comp/Home/Sensors.svelte";
     import Terminal from "@/components/Home/Terminal.svelte";
+    import WateringController from "@/components/Home/WateringController.svelte";
+    
     import Graph from "@/components/Home/Graph.svelte";
     import ActionBtns from "@/components/Home/ActionBtns.svelte";
     import { Notifications } from "@tadashi/svelte-notification";
     import ws from "@/store/ws";
     import { mq } from "@/assets/library/MediaQuery.svelte";
+    import EcController from "../components/Home/EcController.svelte";
 </script>
 
 <main class="col container my-50 gap-40 grow">
-    <div>Firmware version {$ws.ws.v}</div>
+    <!-- <div class="col gap-50" class:row={$mq.xl_}>
+        <WateringController />
+        <EcController />
+    </div> -->
     <Sensors state={$ws.ws} avgDistance={$ws.avgDistance} />
     <div class="col gap-50" class:row={$mq.xl_}>
         <PPMcontroller />
         <ActionBtns />
     </div>
+ 
     <Notifications />
     <div class="col gap-50" class:row={$mq.xl_}>
         <Graph />
