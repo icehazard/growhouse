2afb04c2dfc3145335c373949cba463841fac677
dr3 <dr3@dr3.com>
2023-11-27 16:48:45 +0700

    FIx UI

--- src/components/Home/ActionBtns.svelte
+++ src/components/Home/ActionBtns.svelte
@@ -346,7 +346,7 @@
                 <br />
                 Feed duration is set to {$ws.ws.state.RUN_DURATION / 1000} secs
                 <br />
-                Next feed ~{nextFeed}
+                Next feed in: {$ws.ws.state.FEED_STATE > 0 ? nextFeed : "automatic feeding is off"}
             {/if}
         </div>
     </div>
